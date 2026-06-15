0d8f220a9bef1a541d846377311f3f293bdd3b07
vaani1127 <vprashar_be23@thapar.edu>
2026-06-15 22:22:08 +0530

    ui updates

--- frontend/public/scripts/loader.js
+++ frontend/public/scripts/loader.js
@@ -3,7 +3,7 @@
   loader.id = 'page-loader';
   loader.innerHTML = `
     <div class="loader-brand">
-      <div class="loader-brand-icon">✦</div>
+      <div class="loader-brand-icon"><svg viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width:15px;height:15px;"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg></div>
       Sahayak
     </div>
     <div class="loader-spinner"></div>
